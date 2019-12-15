8abce5f0d6d2efb76197554dbca765472fc9307b
slaytonedwards <50891031+slaytonedwards@users.noreply.github.com>
2019-12-15 16:29:02 -0600

    Add files via upload

---
+++ TestScoresProj/TestScoresProj/Program.cs
@@ -0,0 +1,23 @@
+﻿/* TestScoresProj.cs Author: Slayton Edwards 
+ * Date: 6/10/19
+ * Calculates the average test scores of five tests
+ */
+
+using System;
+using static System.Console;
+
+namespace TestScoresProj
+{
+    class TestScoresProj
+    {
+        static void Main(string[] args)
+        {
+            const int test1 = 75.25;
+            const int test2 = 84.70;
+            const int test3 = 80.25;
+            const int test4 = 95.45;
+            const int test5 = 99.50;
+
+        }
+    }
+}
---
+++ TestScoresProj/TestScoresProj/TestScoresProj.cs
@@ -0,0 +1,46 @@
+﻿/* TestScoresProj.cs Author: Slayton Edwards 
+ * Date: 6/10/19
+ * Calculates the average test scores of five tests
+ */
+
+using System;
+using static System.Console;
+
+namespace TestScoresProj
+{
+    class TestScoresProj
+    {
+        static void Main(string[] args)
+        {
+
+            /* The intergers of the five tests */
+            int test1;
+            int test2;
+            int test3;
+            int test4;
+            int test5;
+            double average;
+
+            /* Insert score of tests with these codes */
+            Console.Write("The score for test 1 is: ");
+            test1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The score for test 2 is: ");
+            test2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The score for test 3 is: ");
+            test3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The score for test 4 is: ");
+            test4 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The score for test 5 is: ");
+            test5 = Convert.ToInt32(Console.ReadLine());
+
+            /* The average of the test score is all the tests added together divided by 5 */
+            average = (test1 + test2 + test3 + test4 + test5) / 5;
+
+            /* The average score for the tests will show here */
+            Console.Write("The Average score is " + "{0:N2}", average);
+
+            /* Read out every command */
+            Console.ReadKey();
+        }
+    }
+}
